fa89140be2d9b896c07fecc9573d66f062ed96ec
surajsalunkhe80 <surajsalunkhe8020@gmail.com>
2024-08-21 14:25:24 +0530

    Add files via upload

---
+++ java/even100.java
@@ -0,0 +1,11 @@
+public class even100 {
+    public static void main(String[] args) {
+        for(int i=1; i<101; i++){
+            if (i%2==0) {
+                
+            
+            System.out.println(i);
+        }}
+    }
+    
+}
