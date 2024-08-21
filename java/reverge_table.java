4e5cff558e0ede9660c520d76610ce0ba834a158
surajsalunkhe80 <surajsalunkhe8020@gmail.com>
2024-08-21 14:29:41 +0530

    Add files via upload

---
+++ java/reverge_table.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class reverge_table {
+    public static void main(String[] args) 
+    {
+        Scanner Scanner = new Scanner (System.in);
+    System.out.println("enter the number");
+int num = Scanner.nextInt();
+
+for(int i=10; i>0; i--) {
+System.out.println(i* num);  
+ }
+}  
+}
